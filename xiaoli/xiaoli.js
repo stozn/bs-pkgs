4ea0f890055832a568a02c687c60ff8cc9ce2526
RiTsu <535543066@qq.com>
2021-06-26 14:40:51 +0000

    update xiaoli/xiaoli.js.

--- xiaoli/xiaoli.js
+++ xiaoli/xiaoli.js
@@ -156,7 +156,31 @@ event dm (user: "黯泣", cont:"^/踢", url, tripcode, req)  => {
 event dm (user: "黯泣", cont:"^/kick", url, tripcode, req)  => {  
     drrr.kick(cont.replace("/kick", "").trim());
 }
-//ban
+//ban人
 event dm (user: "黯泣", cont:"^/ban", url, tripcode, req)  => {  
     drrr.ban(cont.replace("/ban", "").trim());
+}
+//悄悄话
+event dm (user: "", cont:"^/悄悄话", url, tripcode, req)  => {   
+const i=0;
+const a=true;
+drrr.dm(user,"收到！");
+while(i<drrr.users.length){
+  if(drrr.users[i].name == cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search(" ")).trim()) then {
+        a=false;
+        }
+  i++
+}
+
+if (a) then {
+  const cs=0;
+  event join (user) => {
+    if (user == cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search(" ")).trim() && cs<1) then {
+      cs=1;
+      drrr.dm(cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search(" ")).trim(),"有人给你留言：" + cont.replace("/悄悄话", "").trim().slice(cont.replace("/悄悄话", "").trim().search(" ")).trim());
+    }
+  }
+}else{
+drrr.dm(cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search(" ")).trim(),"有人让我悄悄你说："+cont.replace("/悄悄话", "").trim().slice(cont.replace("/悄悄话", "").trim().search(" ")));
+}
 }

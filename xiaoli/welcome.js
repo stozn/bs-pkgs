d131b388872eb6ae62b45d1c9d7121d8f1b06d9a
RiTsu <535543066@qq.com>
2021-06-27 08:01:00 +0000

    update xiaoli/welcome.js.

--- xiaoli/welcome.js
+++ xiaoli/welcome.js
@@ -1,6 +1,6 @@
 //进入房间【欢迎
 event join (user) => {
-  ns =["||进来了就是美少女","||今天也请多多喝水","||你也来喝水啦w"]
+  ns =["|进来了就是美少女","|今天也请多多喝水","|你也来喝水啦w"]
   n = ns[Math.floor(Math.random() * ns.length)];
   m = ["如需帮助请输入：【/帮助】" ]
   drrr.print("/me 欢迎光临@" + user + n)

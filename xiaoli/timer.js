b9f69627c0da80d7ab6d082d10ed77f9edb835f5
RiTsu <535543066@qq.com>
2021-06-27 07:14:16 +0000

    update xiaoli/timer.js.

--- xiaoli/timer.js
+++ xiaoli/timer.js
@@ -13,17 +13,14 @@ timer 60 * 1000{
   f = ["/me ヾ(≧▽≦*)o】现在的时间是【"+checkTime(H)+":"+checkTime(M) +"】每60分钟给大家添水1次，记得好好补充水分哦"]
   ns =["给盖好被子避免着凉","(っ´Ι`)っ翻找冰箱发现（☆▽☆）【"+m+"！！吃了回去继续睡吧w"]
   n = ["/me 【迷迷糊糊的爬起来，"+ ns[Math.floor(Math.random() * ns.length)] ];
-
+  cur = 0
+  bs = ["(:3[___]","(:[___]","([___]","(:3[」_]","(:3[」＿]=:","|[__]∠)_","_(:з」∠)_"]
   if (M ==0)   then { drrr.print(t)}//整点报时
   else
   if (M==30)   then { drrr.print(f)}//60分钟1次【每小时的30分】提醒喝水
   else
   if (M ==15||M==45)   then { drrr.print(n)}  //每30分钟1次【每15/45分】触发【随机活动】
   else
-}
-cur = 0
-timer 60*1000 {
-  ns = ["(:3[___]","(:[___]","([___]","(:3[」_]","(:3[」＿]=:","|[__]∠)_","_(:з」∠)_"]
-  drrr.title("多喝温水"+ns[cur])
-  cur = (cur + 1) % ns.length
+   drrr.title("多喝温水"+ns[cur])
+   cur = (cur + 1) % ns.length
 }

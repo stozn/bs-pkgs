c55cbd537e3bb9cfc0c1fb066d626db8b22c60fc
liuzhixin <9346896+liuzhixin2003@user.noreply.gitee.com>
2021-06-27 11:45:48 +0000

    update xiaoli/whispers.js.

--- xiaoli/whispers.js
+++ xiaoli/whispers.js
@@ -1,11 +1,12 @@
 //悄悄话
-event dm (user: "", cont:"^/悄悄话", url, tripcode, req)  => {   
+event dm (user: "", cont:"^/悄悄话\\s+\\S+\\s+\\S", url, tripcode, req)  => {   
 const i=0;
 const ts=0;
 const a=true;
-const u=cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search(" ")).trim();
-const m=cont.replace("/悄悄话", "").trim().slice(cont.replace("/悄悄话", "").trim().search(" "));
-drrr.dm(user,"收到！");
+const u=cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search("\\s")).trim();
+const m=cont.replace("/悄悄话", "").trim().slice(cont.replace("/悄悄话", "").trim().search("\\s"));
+if(u.slice(0,1)=="@") then {u=u.slice(1);}
+later 2000{drrr.dm(user,"收到！你发送给了："+u+"，内容是："+m);}
 while(i<drrr.users.length && a){
   if(drrr.users[i].name == u) then {
     a=false;

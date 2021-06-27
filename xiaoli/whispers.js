6a333560fb9bb8434f4d44098d43eeac57b915c5
RiTsu <535543066@qq.com>
2021-06-27 05:25:28 +0000

    update xiaoli/whispers.js.

--- xiaoli/whispers.js
+++ xiaoli/whispers.js
@@ -27,14 +27,3 @@ Myfor =()=> {
 }
 Myfor();
 }
-//踢人
-event dm (user: "黯泣", cont:"^/踢", url, tripcode, req)  => {  
-    drrr.kick(cont.replace("/踢", "").trim());
-}
-event dm (user: "黯泣", cont:"^/kick", url, tripcode, req)  => {  
-    drrr.kick(cont.replace("/kick", "").trim());
-}
-//ban
-event dm (user: "黯泣", cont:"^/ban", url, tripcode, req)  => {  
-    drrr.ban(cont.replace("/ban", "").trim());
-}

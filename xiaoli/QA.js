e6bfd2ba04983327faaa2d8c3c9e8b12723b06b6
RiTsu <535543066@qq.com>
2021-06-28 03:45:22 +0000

    update xiaoli/QA.js.

--- xiaoli/QA.js
+++ xiaoli/QA.js
@@ -4,5 +4,5 @@ drrr.print("我今天超乖的！")
 drrr.print("/me 【<(ˉ^ˉ)>")  
 }
 event [me,msg] (user: "", content:"/概率", url, tripcode, req)=> {
-drrr.print("5连概率≈0.005%】\n 4连概率≈0.1%】\n 3连概率≈9%】/n  2连概率≈53%】\n allmiss概率≈38%")
+drrr.print("5连概率≈0.005%】\n 4连概率≈0.1%】\n 3连概率≈9%】\n2连概率≈53%】\n allmiss概率≈38%")
 }

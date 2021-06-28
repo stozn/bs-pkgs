84deae2eddaeb3535c853b8a179a1db68a68eeed
RiTsu <535543066@qq.com>
2021-06-28 06:31:16 +0000

    update xiaoli/help.js.

--- xiaoli/help.js
+++ xiaoli/help.js
@@ -4,8 +4,8 @@ event [me,msg] (user: "", content:"/帮助", url, tripcode, req)  => {
   }
   later 1000
   {drrr.dm(user,"指令列表2：\n抽奖指令（msg/me）：【/抽奖】【/概率】【/中奖历史】\n※所有指令只适配简体字。\n适当抽奖，请勿影响大家的聊天体验" )
- }
+ } }
+ //高级帮助
  event [dm] (user: "", content:"/adh", url, tripcode, req)  => {
   drrr.dm(user,"高级指令：\n 踢人：【/踢】【/kick】\n ban人：【/ban】\n ban人【/ban】\n让小粒说：【/说】\n房间主题：【/主题】")
- }
  }

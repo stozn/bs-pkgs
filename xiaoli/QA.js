409d0c4f4e2ac80a013798822c06c6a6cde005b7
RiTsu <535543066@qq.com>
2021-06-30 07:28:56 +0000

    update xiaoli/QA.js.

--- xiaoli/QA.js
+++ xiaoli/QA.js
@@ -1,7 +1,13 @@
 //乖嘛
-event [me,msg] (user: "黯泣", content:"小粒今天乖嘛", url, tripcode, req)=> {
-drrr.print("我今天超乖的！")
+event [me,msg] (user: "", content:"小粒今天乖嘛", url, tripcode, req)=> {
+    if user==["黯泣"]
+  then 
+{drrr.print("我今天超乖的！")
 drrr.print("/me 【<(ˉ^ˉ)>")  
+}
+else
+{drrr.print("才不告诉你呢！")
+drrr.print("/me 【<(ˉ^ˉ)>") }
 }
 //概率
 event [me,msg] (user: "", content:"/概率", url, tripcode, req)=> {

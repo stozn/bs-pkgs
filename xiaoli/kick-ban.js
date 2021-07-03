4aa199e73292a7da5fd8c799cb79e20d58b3fd91
liuzhixin <liuzhixin2003@outlook.com>
2021-07-03 08:42:26 +0000

    update xiaoli/kick-ban.js.

--- xiaoli/kick-ban.js
+++ xiaoli/kick-ban.js
@@ -1,11 +1,17 @@
 //踢人
-event dm (user: "黯泣", cont:"^/踢", url, tripcode, req)  => {  
-    drrr.kick(cont.replace("/踢", "").trim());
+event dm (user: "黯泣", cont:"^/踢\\s+\\S") => {  
+u=cont.replace("/踢", "").trim()
+if(u.slice(0,1)=="@") then {u=u.slice(1)}
+drrr.kick(u);
 }
-event dm (user: "黯泣", cont:"^/kick", url, tripcode, req)  => {  
-    drrr.kick(cont.replace("/kick", "").trim());
+event dm (user: "黯泣", cont:"^/kick\\s+\\S") => {  
+u=cont.replace("/kick", "").trim()
+if(u.slice(0,1)=="@") then {u=u.slice(1)}
+drrr.kick(u);
 }
 //ban
-event dm (user: "黯泣", cont:"^/ban", url, tripcode, req)  => {  
-    drrr.ban(cont.replace("/ban", "").trim());
+event dm (user: "黯泣", cont:"^/ban")  => {  
+u=cont.replace("/ban", "").trim()
+if(u.slice(0,1)=="@") then {u=u.slice(1)}
+drrr.ban(u);
 }

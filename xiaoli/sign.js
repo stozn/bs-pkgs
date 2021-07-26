a84be5eef23cb301874db9865ea2cc9e591496c9
liuzhixin <liuzhixin2003@outlook.com>
2021-07-26 14:05:58 +0000

    update xiaoli/sign.js.

--- xiaoli/sign.js
+++ xiaoli/sign.js
@@ -8,6 +8,17 @@ timer 24*60*60*1000 {
     a++
   }
 }
+//排行榜
+sort = () =>{
+  users.sort((a,b) => b.coins - a.coins)
+  let pm=users
+  if users.length >10 then pm=pm.slice(0,10)    //截取排名前10的用户
+  let p=pm.reduce((a,x,y) => a=a+"\n"+(y+1)+"."+x.name+"\t"+x.coins+"硬币","排行榜 总用户:"+users.length+"人")
+  return p
+ }
+event [msg, me, dm] (user, cont: "^/排行榜") => {
+  drrr.print(sort())
+  }
 //签到
 event [msg, me, dm] (user, cont: "^/签到") => { 
  const yb=Math.floor(Math.random() * 3)+1
@@ -20,14 +31,28 @@ event [msg, me, dm] (user, cont: "^/签到") => {
   users[n].coins+=yb
   users[n].check=false
   drrr.print(user+"签到成功，硬币+"+yb+"，现在的硬币数量为"+users[n].coins+"。")
-  } else { drrr.print(user+"今天已经签过到了，现在的硬币数量为"+users[n].coins+"。")
+  } else { drrr.print( "/me @"+ user +"今天已经签过到了，现在的硬币数量为"+users[n].coins+"。")
 }
   }
+//删除
+event [msg, me, dm] (user, cont: "^/删除\\s+\\S") => { 
+  if admins.some(a => a==user) then {
+   del=cont.replace("/删除", "").trim();
+   let n=users.findIndex(u => u.name == del)
+   if (n == -1) then {
+     drrr.dm(user,"●该用户不存在")
+  } else {
+    users.splice(n,1)
+    drrr.dm(user,"●成功删除用户"+del)
+  }
+   }
+}
 //导出
 event [msg, me, dm] (user, cont: "^/导出") => { 
   if admins.some(a => a==user) then {
    print(users)
-   print(false)
+   print(0)
+   drrr.dm(user,"●成功导出数据")
    }
 }
 //导入
@@ -35,17 +60,35 @@ event [msg, me, dm] (user, cont: "^/导入\\s+\\S") => {
   if admins.some(a => a==user) then {
     data=cont.replace("/导入", "").trim();
     dt=JSON.parse(data)      //支持分批导入，以解决drrr字数限制
-    users=users.concat(dt)
-   }
-}  
-//排行榜
-event [msg, me, dm] (user, cont: "^/排行榜") => {
-  users.sort((a,b) => b.coins - a.coins)
-  let pm=users
-  if users.length >10 then pm=pm.slice(0,10)    //截取排名前10的用户
-  let p=pm.reduce((a,x,y) => a=a+"\n"+(y+1)+"."+x.name+"\t"+x.coins+"硬币","")
-  drrr.print(p)
+    if dt==false then {
+      drrr.dm(user,"●数据有误")
+    }else {
+    users=users.concat(dt)   
+    var r = []
+    var d = []
+    let l = users.length
+    for  i = 0; i < l; i++  {
+     for  j = i + 1; j < l; j++ {
+      if (users[i].name === users[j].name) then {
+        j = i+1
+        d.push(users[i])
+        i++
+      } 
+    }  
+    r.push(users[i])
   }
+  if (r.length==users.length) then{
+    drrr.dm(user,"●导入数据成功")
+  }else {
+    users=r
+    let de=d.reduce((a,x,y) => a=a+"\n"+(y+1)+"."+x.name+"\t"+x.coins+"硬币","")
+    drrr.dm(user,"●更新数据成功，已删除旧数据:")
+    later 500 drrr.dm(user,de)
+  }         
+ later 1000 drrr.dm(user,sort())
+    }
+  }
+}  
 //注文
 event [me,msg] (user, cont:"^/注文\\s+\\S")  => {
 var r=cont.replace("/注文", "").trim();
@@ -58,9 +101,9 @@ let n=users.findIndex(u => u.name == user)
   if (n == -1) then {
   users.push({ name: user,coins: 0,check: true})
   n=users.length-1
-  drrr.print(user+"很抱歉，注文功能需要花费1硬币，您的硬币数为"+users[n].coins+"，请签到获取硬币。")
+  drrr.print(user+"/me @"+ user +"很抱歉，注文功能需要花费1硬币，您的硬币数为"+users[n].coins+"，请签到获取硬币。")
 } else if (users[n].coins == 0) then {
-  drrr.print(user+"很抱歉，注文功能需要花费1硬币，您的硬币数为"+users[n].coins+"，请签到获取硬币。")
+  drrr.print(user+"/me @"+ user +"很抱歉，注文功能需要花费1硬币，您的硬币数为"+users[n].coins+"，请签到获取硬币。")
 } else {
   users[n].coins--
   drrr.print("/me @"+ user +" 您使用了1硬币，现在您的硬币数量为"+users[n].coins+"，["+r+"]马上就好，请稍等一分钟" );

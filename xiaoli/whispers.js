b1ff79070648eb793c3f2e33486b253b3c4a44f9
liuzhixin <liuzhixin2003@outlook.com>
2021-08-15 03:59:45 +0000

    update xiaoli/whispers.js.

--- xiaoli/whispers.js
+++ xiaoli/whispers.js
@@ -1,17 +1,13 @@
 //悄悄话
-event dm (user: "", cont:"^/悄悄话\\s+\\S+\\s+\\S", url, tripcode, req)  => {   
-//设置
-const kp=60*24*3 //保存时间=kp*rf毫秒
-const rf=60*1000 //刷新频率=rf毫秒
-
-//主体功能
-const i=0
-const ts=0
-const a=true
+const admins=["Ancy.WWeeo","Robot/23Cc","unica/qOLU","YtIMnsXOBE"]   //设置管理员
+let msgs=[]
+event dm (user, cont:"^/悄悄话\\s+\\S+\\s+\\S")  => {   
+let i=0
+let a=true
 const u=cont.replace("/悄悄话", "").trim().slice(0,cont.replace("/悄悄话", "").trim().search("\\s")).trim()
 const m=cont.replace("/悄悄话", "").trim().slice(cont.replace("/悄悄话", "").trim().search("\\s"))
 if(u.slice(0,1)=="@") then {u=u.slice(1);}
-later 2000{drrr.dm(user,"收到！你发送给了："+u+"，内容是："+m);}
+later 2000 drrr.dm(user,"收到！你发送给了："+u+"，内容是："+m)
 while(i<drrr.users.length && a){
   if(drrr.users[i].name == u) then {
     a=false;
@@ -19,17 +15,28 @@ while(i<drrr.users.length && a){
     }
   i++
 }
-Myfor =()=> {
-  const j=0; 
-  while(j<drrr.users.length && a){
-    if(drrr.users[j].name == u) then {
-      a=false;
-      drrr.dm(u,"有人让我悄悄跟你说："+m);
-     }
-   j++
-   }
- ts++;
- if (a && ts<kp) then{setTimeout(Myfor, rf);}  
+  if a  then {
+    let n=msgs.findIndex(u => u.tou == u)
+    if n>0 then msgs[n].msg.push(m)
+    else msgs.push({tou: u ,msg: [m]})
+  }
 }
-Myfor();
+event join (user) => {
+  let n=msgs.findIndex(u => u.tou == user)
+  if n>(-1) then {
+  let j=0
+  Myfor =()=> {
+   drrr.dm(user,"有人让我悄悄跟你说："+msgs[n].msg[j]);
+   j++
+   if j<msgs[n].msg.length then setTimeout(Myfor, 1500) 
+   }
+   Myfor()
+  }
+}
+//管理员查看
+event [msg, me, dm] (user, cont: "^/悄悄话$", url, tc) => { 
+  if admins.some(a => a==tc) then {
+   let dt=JSON.stringify(msgs)
+   drrr.dm(user,dt)
+   }
 }

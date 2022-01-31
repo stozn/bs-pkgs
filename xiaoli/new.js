5a88350cf2c799cc9d55128fa59af671e12a8254
liuzhixin <liuzhixin2003@outlook.com>
2022-01-31 04:04:08 +0000

    update xiaoli/new.js.

--- xiaoli/new.js
+++ xiaoli/new.js
@@ -20,7 +20,7 @@ onTimeDo = (h, m, s, callback) => {
     delta += (delta < 0) * (interval * 1000)
     later delta {
       callback()
-      loop()
+      later 2000 loop()
     }
   }
   loop()
@@ -34,28 +34,29 @@ n.setSeconds(0)
 event[msg, me, dm](user, cont: "^/倒计时") => {
     now = new Date()
   if now.getDate()==31 then{
-    delta = 86400000 + n.gecime() - Date.now()
+    delta = 86400000 + n.getTime() - Date.now()
     h=c(Math.floor(delta/(3600*1000)))
     m=c(Math.floor((delta%(3600*1000))/(60*1000)))
     s=c(Math.floor(((delta%(3600*1000))%(60*1000))/1000))  
-    drrr.print("现在的时间是：  "+tNow()+"\n距离2022还有： " +h+"时"+m+"分"+s+"秒")
+    drrr.print("现在的时间是：  "+tNow()+"\n距离虎年还有： " +h+"时"+m+"分"+s+"秒")
   }
 }
 
 event join (user) => {
- drrr.dm(user,"查看新年倒计时请回复 /倒计时 \n0点有红包发送")
+ drrr.dm(user,"查看新年倒计时请回复 /倒计时 \n※整点有红包发送")
 }
 onTimeDo( -1, 0, 0, () => {
  now = new Date()
   if now.getDate()==31 then{
-    delta = 86400000 + n.gecime() - Date.now()
-    h=t(Math.floor(delta/(3600*1000)))
-    m=t(Math.floor((delta%(3600*1000))/(60*1000)))
-    s=t(Math.floor(((delta%(3600*1000))%(60*1000))/1000))  
-    drrr.print("距离2022还有： " +h+"时"+m+"分"+s+"秒")
+    delta = 86400000 + n.getTime() - Date.now()
+    h=c(Math.floor(delta/(3600*1000)))
+    m=c(Math.floor((delta%(3600*1000))/(60*1000)))
+    s=c(Math.floor(((delta%(3600*1000))%(60*1000))/1000))  
+    drrr.print("红包来啦！距离虎年还有：\n " +h+"时"+m+"分"+s+"秒")
+    drrr.print("/me/发红包 10 1000")
   }
-  if now.getHours()==0 && now.getDate()==0  then {
+  if now.getHours()==0 && now.getDate()==1  then {
     drrr.print("新年快乐！！！")
-    drrr.print("/发红包 10 1000")
+    drrr.print("/me/发红包 20 10000")
   } 
 })

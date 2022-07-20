b350a1b22aa6f9365eee34e92124ee8a557558c4
liuzhixin <liuzhixin2003@outlook.com>
2022-07-20 16:18:35 +0000

    update xiaoli/sign.js.

--- xiaoli/sign.js
+++ xiaoli/sign.js
@@ -1582,7 +1582,7 @@ event[msg, me, dm](user, cont: "^/导入", url, tc) => {
             for x of input{
                 if users.some(m => (m.name == x.name || m.tc == x.tc) && m.uid != x.uid) then{
                     b.push(x)
-                }else if users.some(m => m.uid = x.uid) then {
+                }else if users.some(m => m.uid == x.uid) then {
                     n = users.findIndex(i => i.uid == x.uid) -1
                     drrr.dm(user, "已删除" +users[n].name)
                     users.splice(n, 1)

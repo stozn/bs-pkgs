d9ca7439975bacd96972b69b84627f47aea7d1bd
liuzhixin <liuzhixin2003@outlook.com>
2022-11-09 13:41:40 +0000

    update xiaoli/timer.js.
    Signed-off-by: liuzhixin <liuzhixin2003@outlook.com>

--- xiaoli/timer.js
+++ xiaoli/timer.js
@@ -75,3 +75,41 @@ onTimeDo(-1, 5, 0, () => {
         drrr.title("多喝温水" + sample(bs))
     }
 })
+//聊天机器人
+// http://api.qingyunke.com
+chrome.permissions.request({
+  origins: ['*'],
+}, (ok) => drrr.log("request: " + ok));
+
+chunkString = (size, str) => {
+  pos = 0; chunks = []
+  while pos < str.length {
+    chunks.push(str.substr(pos, size))
+    pos += size
+  }
+  chunks
+}
+
+batch_print = msg => {
+  buffer = ""; msgs = []
+  msg.split("。").forEach(ctx => {
+    if ctx.length then
+      chunkString(120, ctx + "。").forEach(chk => {
+        if (buffer.length + chk.length) > 120
+        then { msgs.push(buffer); buffer = chk }
+        else { buffer = buffer + chk; }
+      })
+  })
+  if(buffer.length) then msgs.push(buffer)
+  msgs.reverse();
+  msgs.forEach(m => drrr.print("/me" + m))
+}
+
+event [msg,me] (user, cont:'^#') => {
+  msg = cont.substr(1)
+  url='http://api.qingyunke.com/api.php?key=free&appid=0&msg='
+     + encodeURI(msg)
+  fetch(url)
+    .then(r=>r.json())
+    .then(r=>batch_print(("@" + user + " " + r.content).replaceAll('{br}', '\n').replace('菲菲','小粒').replace('傻逼','小可爱')));
+}
